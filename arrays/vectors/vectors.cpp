40ac4814f08ff905099a485c9bb5bb800bfe4259
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-09-16 14:52:26 +0530

    iterator of vector

--- arrays/vectors/vectors.cpp
+++ arrays/vectors/vectors.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+void printVector(vector<int> &v) {
+  for (int i = 0; i < v.size(); i++) {
+    cout << v[i] << ",";
+  }
+}
+
 int main() {
   vector<int> vec1;
   int element;
@@ -9,11 +16,20 @@ int main() {
     cin >> element;
     vec1.push_back(element);
   }
-  for (int i = 0; i < (vec1.size()); i++)
-  {
-    cout<<vec1[i];
-  }
-  
+
+  printVector(vec1);
+  vec1.pop_back();
+  cout << "---------------";
+  printVector(vec1);
+  cout << "---------------";
+  vec1[2] = 9;
+  printVector(vec1);
+  cout << "---------------";
+
+  vector<int>::iterator iter = vec1.begin();
+  vec1.insert(iter, 34, 33);
+  printVector(vec1);
+  cout << "---------------";
 
   return 0;
 }
