06afbce7fb7c55e7cbf8c92883ffb833d0a61f39
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-11-21 13:56:00 +0530

    TSP

--- DAA/jobScheudling.cpp
+++ DAA/jobScheudling.cpp
@@ -1,68 +1,57 @@
 // C++ code for the above approach
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 #include <algorithm>
 #include <iostream>
 using namespace std;
 
-
 struct Job {
-
-	char id; 
-	int dead; 
-	int profit;
+  char id;
+  int dead;
+  int profit;
 };
 
-
-bool comparison(Job a, Job b)
-{
-	return (a.profit > b.profit);
+bool comparison(Job a, Job b) {
+  return (a.profit > b.profit);
 }
 
+void printJobScheduling(Job arr[], int n) {
+  sort(arr, arr + n, comparison);
 
-void printJobScheduling(Job arr[], int n)
-{
-	
-	sort(arr, arr + n, comparison);
+  int result[n];
+  bool slot[n];
 
-	int result[n];
-	bool slot[n]; 
-	
-	for (int i = 0; i < n; i++)
-		slot[i] = false;
+  for (int i = 0; i < n; i++)
+    slot[i] = false;
 
-	
-	for (int i = 0; i < n; i++) {
-		
+  for (int i = 0; i < n; i++) {
+    for (int j = min(n, arr[i].dead) - 1; j >= 0; j--) {
+      if (slot[j] == false) {
+        result[j] = i;
+        slot[j] = true;
+        break;
+      }
+    }
+  }
 
-		for (int j = min(n, arr[i].dead) - 1; j >= 0; j--) {
-			
-			if (slot[j] == false) {
-				result[j] = i; 
-				slot[j] = true; 
-				break;
-			}
-		}
-	}
-
-	// Print the result
-	for (int i = 0; i < n; i++)
-		if (slot[i])
-			cout << arr[result[i]].id << " ";
+  // Print the result
+  for (int i = 0; i < n; i++)
+    if (slot[i])
+      cout << arr[result[i]].id << " ";
 }
 
+int main() {
+  Job arr[] = {{'a', 2, 100},
+               {'b', 1, 19},
+               {'c', 2, 27},
+               {'d', 1, 25},
+               {'e', 3, 15}};
 
-int main(){
+  int n = sizeof(arr) / sizeof(arr[0]);
+  cout << "Following is maximum profit sequence of jobs "
+       << "\n";
 
-	Job arr[] = { { 'a', 2, 100 },
-				{ 'b', 1, 19 },
-				{ 'c', 2, 27 },
-				{ 'd', 1, 25 },
-				{ 'e', 3, 15 } };
-
-	int n = sizeof(arr) / sizeof(arr[0]);
-	cout << "Following is maximum profit sequence of jobs "<<"\n";
-
-	printJobScheduling(arr, n);
-	return 0;
+  printJobScheduling(arr, n);
+  return 0;
 }
-// time complexity of job scheudling algorithm is O(N^2) and auxilary space is O(N)
+// time complexity of job scheduling algorithm is O(N^2) and auxilary space is O(N)
