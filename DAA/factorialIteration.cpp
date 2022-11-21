e908d9e2d38cb62ccfbde19414d6f9a0edf0487a
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-11-21 16:23:40 +0530

    factorial iteration

---
+++ DAA/factorialIteration.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+long long factorial(int n) {
+  if (n == 0) return 1;
+  long long fact = 1;
+  for (int i = 1; i <= n; i++) {
+    fact = fact * i;
+  }
+  return fact;
+}
+
+int main() {
+  int n;
+
+  cout << "Enter a positive integer: ";
+  cin >> n;
+
+  cout << "Factorial of " << n << " = " << factorial(n) << endl;
+
+  //   cout<<INT16_MAX<<"__"<<INT64_MAX<<endl;
+
+  return 0;
+}
