52aae5452365e5207b380c7b5eb2a474eee0aef0
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-11-29 17:43:10 +0530

    fib

--- DAA/fibonacciRecursive.cpp
+++ DAA/fibonacciRecursive.cpp
@@ -1,24 +1,47 @@
 // Fib rec
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// void printFibonacci(int n) {
+//   static int n1 = 0, n2 = 1, n3;
+//   if (n > 0) {
+//     n3 = n1 + n2;
+//     n1 = n2;
+//     n2 = n3;
+//     cout << n3 << " ";
+//     printFibonacci(n - 1);
+//   }
+// }
+// int main() {
+//   int n;
+//   cout << "Enter the number of elements: ";
+//   cin >> n;
+//   cout << "Fibonacci Series: ";
+//   cout << "0 "
+//        << "1 ";
+//   printFibonacci(n - 2);
+//   return 0;
+// }
+
+#include <bits/stdc++.h>
 using namespace std;
 
-void printFibonacci(int n) {
-  static int n1 = 0, n2 = 1, n3;
-  if (n > 0) {
-    n3 = n1 + n2;
-    n1 = n2;
-    n2 = n3;
-    cout << n3 << " ";
-    printFibonacci(n - 1);
-  }
+void fib(vector<int>& v, int n) {
+  if (n <= 1) return;
+  v.push_back(v.back() + v[v.size() - 2]);
+  fib(v, n - 1);
 }
+
 int main() {
   int n;
-  cout << "Enter the number of elements: ";
+  cout<<"Enter number: ";
   cin >> n;
-  cout << "Fibonacci Series: ";
-  cout << "0 "
-       << "1 ";
-  printFibonacci(n - 2);
+  vector<int> v;
+  v.push_back(0);
+  v.push_back(1);
+  fib(v, n);
+  for (auto p : v) {
+    cout << p << " ";
+  }
   return 0;
 }
